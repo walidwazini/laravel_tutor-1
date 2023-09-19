bca6eb71d672285cb6eba23e98484fcb3a15d34f
Walid Wazini <wazini25@gmail.com>
2023-09-19 15:33:39 +0800

    simple intro to routing

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,101 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/', function () {
+    return 'Welcome to Laravel';
+});
+
+Route::get('/cuaca', function () {
+    $demoData = [
+        [
+            "id" => "k1",
+            "lokasi" => "Gombak",
+            'suhu' => 25,
+            'keadaan' => "mendung",
+        ],
+        [
+            "id" => "k2",
+            "lokasi" => "Shah Alam",
+            'suhu' => 32,
+            'keadaan' => "panas",
+        ],
+    ];
+    return response()->json($demoData);
+});
+
+Route::get('/cuaca/{id}', function ($id) {
+    return response()->json(
+        ["id" => $id]
+    );
+});
+
+Route::post('/cuaca', function (Request $request) {
+    return response()->json($request);
+});
+
+
+Route::group(['prefix' => '/weather'], function () {
+    Route::get('/', function () {
+        $demoData = [
+            [
+                "id" => "p1",
+                "location" => "sevilla",
+                "condition" => "sunny",
+            ],
+            [
+                "id" => "p2",
+                "location" => "madrid",
+                "condition" => "cloud",
+            ],
+            [
+                "id" => "p3",
+                "location" => "granada",
+                "condition" => "raining",
+            ],
+        ];
+
+        return response()->json($demoData);
+    });
+
+    Route::get('/{id}', function ($id) {
+        try {
+            //code...
+            $demoData = [
+                [
+                    "id" => "p1",
+                    "location" => "sevilla",
+                    "condition" => "sunny",
+                ],
+                [
+                    "id" => "p2",
+                    "location" => "madrid",
+                    "condition" => "cloud",
+                ],
+                [
+                    "id" => "p3",
+                    "location" => "granada",
+                    "condition" => "raining",
+                ],
+            ];
+            $res = [];
+
+            foreach ($demoData as $key => $value) {
+                if ($id == $value['id']) {
+                    $res = $value;
+                    break;
+                } else if ($id != $value['id']) {
+                    return response()->json(['message' => 'Data not found'], 404);
+                }
+            }
+            return response()->json($res);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    });
 });
